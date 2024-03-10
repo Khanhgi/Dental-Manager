576c7461104ef44e4619674a47baf2cd6d765177
Khánh Võ An <voankhanh2k2@gmail.com>
2024-03-10 11:01:22 +0700

    TP

--- Program.cs
+++ Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<QlkrContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("QLKR")));
 
+//Test
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
